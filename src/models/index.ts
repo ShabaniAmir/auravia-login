f6ec48ef54a9e051aa26c94cb7da468cbc48ede6
Amir Shabani <shabani.a.amir@gmail.com>
2023-03-15 10:33:32 -0400

    Fix authentication type exports

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,9 @@
+import { Tenant, User } from '@prisma/client';
 import { TenantModel } from './tenant';
 import { UserModel } from "./user";
 export {
     UserModel as User,
     TenantModel as Tenant,
 }
+export type UserType = User;
+export type TenantType = Tenant;
