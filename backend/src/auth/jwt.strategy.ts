77def8c22bd024f416be43544fd465d87ba1af93
Amir Shabani <shabani.a.amir@gmail.com>
2023-03-16 20:44:29 -0400

    Fix JWT strategy not verifying user

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -1,10 +1,11 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor() {
+    constructor(private readonly usersService: UsersService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
@@ -13,6 +14,11 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     }
 
     async validate(payload: { id: number, email: string }) {
+        const user = await this.usersService.findUserByEmail(payload.email);
+        if (!user) {
+            throw new HttpException('Unauthorized access', 401)
+        }
+
         return {
             id: payload.id,
             email: payload.email,
